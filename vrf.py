94fbc23eb4272105980537593d6bf9c713f646ff
Anais5s <anaisboisson@gmail.com>
2025-04-07 14:24:25 +0200

    vrf bis

--- vrf.py
+++ vrf.py
@@ -15,5 +15,14 @@ def config_vrf(router_id,voisin_id,data,nom_client):
     commands.append(f"rd {as_self}:{as_neighbour}")
     commands.append(f"route-target export {as_self}:{nom_client[3:]}") #car nos noms de clients sont VPN1, VPN2, ...
     commands.append(f"route-target import {as_self}:{nom_client[3:]}")
+    commands.append("address-family ipv4")
+    commands.append("exit-address-family")
 
-
+def config_vrf_interface(interface,nom_client):
+    '''
+    interface FastEthernet0/0
+    vrf forwarding Client_1
+    '''
+    commands = ["conf t"]
+    commands.append(f"interface {interface}")
+    commands.append(f"vrf definition {nom_client}")
