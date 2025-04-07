4eaac0e2f3fbfa635f9540e7d3ee30df870c7e8c
cb <charlesbouquet1011@gmail.com>
2025-04-07 14:27:12 +0200

    getInterfaceClients

--- MP-BGP.py
+++ MP-BGP.py
@@ -15,9 +15,24 @@ def routeur_est_PE(routeur,numAs,data):
         longueur=len(liste)
         if longueur>max:
             max=longueur
+        if max>=3: break #pour aller plus vite
     
     return max>=3 #si on a une liste de longueur 3 dans les voisins ex [R18,0,VPN1] alors c'est un PE
 
 def getNomClient(routeur,numAS,data,interface):
     return data[numAS]["routeurs"][routeur][interface][2]
+
+
+def getInterfacesClient(routeur,numAs,data):
+    """
+    Renvoie une liste d'interfaces associées au nom du client
+    """
+    voisins=data[numAs]["routeurs"][routeur]
+    retour=[]
+    for interface,liste in voisins.items():
+        if len(liste)==3:
+            retour.append([interface,getNomClient(routeur,numAs,data,interface)])
+    
+
+
     
