e756f3062cc3ca33e2881e89316078c4dc617a56
cb <charlesbouquet1011@gmail.com>
2025-04-07 15:27:39 +0200

    MP BGP

--- MP-BGP.py
+++ MP-BGP.py
@@ -3,6 +3,8 @@ Configure MP-BGP Sur les routeurs PE seulement !!
 
 """
 
+
+
 def routeur_est_PE(routeur,numAs,data):
     """
     data:fichier d'intention
@@ -31,8 +33,57 @@ def getInterfacesClient(routeur,numAs,data):
     retour=[]
     for interface,liste in voisins.items():
         if len(liste)==3:
-            retour.append([interface,getNomClient(routeur,numAs,data,interface)])
-    
+            CE=liste[0]
+            retour.append([getNomClient(routeur,numAs,data,interface),CE,interface])
+            
+
+
+def getIpVoisin(routeur,voisin,config):
+    """
+    Renvoie l'IP qu'à un voisin sur l'interface à laquelle est connectée notre routeur
+    config est le dictionnaire de config
+    """
+    return config[voisin]["ip_et_co"][routeur][1]
+
+def configMp_BGP_routeur(routeur,AS,data,config):
+    from BGP import get_as_for_router
 
 
     
+    commandes=[]
+    commandes.append("conf t")
+        
+    commandes.append(f"router bgp {AS}")
+        
+        #config vpnv4
+    commandes.append("address-family vpnv4")
+    for PEVoisin in trouve_PE_AS(AS,data):
+        loopbackVoisin=getIpLoopback(PEVoisin,config)
+        commandes.append(f"neighbor {loopbackVoisin} activate")
+        commandes.append(f"neighbor {loopbackVoisin} send-community extended")
+    commandes.append("exit-address-family")  
+
+    #config vrf avec les clients
+    for client,CE,interface in getInterfacesClient(routeur,AS,data):
+        commandes.append(f"address-family ipv4 vrf {client}")
+        ipvoisin=getIpVoisin(routeur,CE,config)
+        asVoisin=get_as_for_router()
+        commandes.append(f"neighbor {ipvoisin} remote-as {asVoisin}")
+        commandes.append(f"neighbor {ipvoisin} activate")
+        commandes.append('exit-adress-family')
+    
+
+    commandes.append("end")
+    return commandes
+
+
+def getIpLoopback(routeur,config):
+    return config[routeur]["loopback"]
+
+def trouve_PE_AS(numAs,data):
+    liste=[]
+    for routeur in data[numAs]["routeurs"]:
+        if routeur_est_PE(routeur,numAs,data):
+            liste.append(routeur)
+    
+
